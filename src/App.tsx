ea56cf9819884e524bac57370ca446e0c13aef8d
vearvip <vear.vip@qq.com>
2020-02-06 09:57:15 +0800

    🐴 - 添加状态管理

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,23 @@ import { IconStyle } from './assets/iconfont/iconfont';
 import routes from './routes';
 import { HashRouter } from 'react-router-dom';
 import { renderRoutes } from 'react-router-config';//renderRoutes 读取路由配置转化为 Route 标签
+import { Provider } from 'react-redux'
+import store from './store/index'
 
 
 
 export default memo(function App() {
   return (
-    <HashRouter>
-      <IconStyle></IconStyle>
+    <Provider store={store}>
+      <HashRouter>
+        <IconStyle></IconStyle>
 
-      <GlobalStyle></GlobalStyle>
+        <GlobalStyle></GlobalStyle>
 
-      {
-        renderRoutes(routes)
-      }
-    </HashRouter>
+        {
+          renderRoutes(routes)
+        }
+      </HashRouter>
+    </Provider>
   )
 })
